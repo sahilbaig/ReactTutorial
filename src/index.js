8bffc828c00ef0cafff033ff2451bcca399dfd9c
sahilbaig <sahilbaig.me@gmail.com>
2021-12-06 23:16:54 +0530

    ss

--- src/index.js
+++ src/index.js
@@ -1,24 +1,49 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css" ;
+
+
+const books=[
+    {   
+        id:1,
+        title:"Book 1",
+        author:"Author 1",
+        img:"https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/Trollface_non-free.png/330px-Trollface_non-free.png"
+    },
+    
+    {
+        id:2,
+        title: "Book2",
+        author: "Author 2",
+        img : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR6SJ3XPQ7Ysv-y1JZSVOAOUvGOCIKT7TWk7g&usqp=CAU"
+    }
+
+
+
+]
+
 function BookList(){
     return (
         <section >
-            <Book/>
+           {books.map((book)=>{
+              return(
+                    <Book key={book.id} book={book}></Book>
+              )
+           })}
+            
         </section>
     );
-}
+ }
 
-const author="I am a author";
-const Book=() => {
-    const title="hello there"
+const Book=(props) => {
+    const {img,title,author}=props.book
     return (
         <div>
             <h1>{title}</h1>
-            <img src="https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/Trollface_non-free.png/330px-Trollface_non-free.png" 
+            <img src={img} 
             alt="why you do this to me"></img>
-            <h4>{author.toUpperCase()}</h4>
-            <h4>Price:{5+5}</h4>
+            <h4>{author}</h4>
+            {props.children}
         </div>
     )
 }
